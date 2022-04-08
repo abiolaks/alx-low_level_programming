e0995d95f70e2554a86420403764b5bfcf2912f4
abiolaks <kingsleylawani@gmail.com>
2022-04-08 10:59:11 -0700

    working with conditions

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ *
+ * rand - generate pseudo-random numbers
+ *
+ * srand - seed for reproducibility of code a time
+ */
+int main(void)
+{
+	int n;
+	int las_num;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	las_num = n % 10;
+
+	printf("Last digit of %d is %d ", n, las_num);
+
+	if (las_num > 5)
+	{
+		printf(" and is greater than 5\n");
+	}
+	else if (las_num == 0)
+	{
+		printf(" and is 0\n");
+	}
+	else if(las_num < 6 && las_num !=0)
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+	return (0);
+}
