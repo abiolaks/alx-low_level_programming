95a005dc9c8a24af9ddf32e1eca6a8746bc4a899
abiolaks <kingsleylawani@gmail.com>
2022-04-08 21:03:06 -0700

    reverse lower case letter

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * putchar - print character
+ *
+ * Description: The below code prints the lower case
+ * alphabet in reverer using only the putchar function.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 0172; i >= 0141; --i)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
