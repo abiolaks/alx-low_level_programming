68ec061c7a60bbee1d36e35b5b32c823bd2e1218
abiolaks <kingsleylawani@gmail.com>
2022-04-12 16:12:00 -0700

    printing lowwer case letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point of the program
+ * Return: always return succes for 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
