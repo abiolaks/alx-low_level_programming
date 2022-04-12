d22c2de6dd003fceaebf789e04f7b53ae0c5f921
abiolaks <kingsleylawani@gmail.com>
2022-04-12 15:22:54 -0700

    print using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,16 @@
+#include "main.h"
 /**
- * main - Entry point
- *
- * _putchar - dispaly character to the output using system call.
- *
- * @c: The character to print
- *
- * Return: If the value pass is (0) means program successful
- *
+ * main - entry point
+ * Return: always sucess(0)
  */
-int _putchar(char c);
-
 int main(void)
 {
 	char c[] = "_putchar";
 	int i;
 
-	i = 0;
-	while (c[i] != '\0')
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(c[i]);
-		i++;
 	}
 	_putchar('\n');
 	return (0);
