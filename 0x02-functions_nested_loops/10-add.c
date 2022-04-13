abd007346374e50556b4dbafaccaf25180a8e9b3
abiolaks <kingsleylawani@gmail.com>
2022-04-12 21:59:58 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - This takes two numbers and return the ouput
+ * Return: return the sum
+ * @a: first number
+ * @b: second number
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
