3abc05fc21da43cbd778c2e907bd020cb4e33e39
abiolaks <kingsleylawani@gmail.com>
2022-04-12 22:14:08 -0700

    print from one to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * print_to_98 - this function prints all natural numbers
+ * from n to 98
+ * @n: get the number to test
+ */
+void print_to_98(int n)
+{
+	for (i = 0; i < 99; i++)
+	{
+		printf("%d, ", i);
+	}
+
+}
