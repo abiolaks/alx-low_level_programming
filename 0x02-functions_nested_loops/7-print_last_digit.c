f09351697a70c63e910ac24314889a443188ab63
abiolaks <kingsleylawani@gmail.com>
2022-04-12 21:22:07 -0700

    print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit - This function prints the last digit of a number
+ * Return: returns zero for success
+ * @i: The test value
+ */
+int print_last_digit(int i)
+{
+	int ldgt;
+
+	ldgt = i % 10;
+	return (ldgt);
+}
