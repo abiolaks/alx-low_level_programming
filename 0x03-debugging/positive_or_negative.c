74227844badfed612ff35af043cdeebdebf8257e
abiolaks <kingsleylawani@gmail.com>
2022-04-14 02:45:51 -0700

    Debug 002 : test when number is 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - This display wether a number is -ve or +ve
+ */
+void positive_or_negative(int i)
+{
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+		printf("%d is negative\n", i);
+}
