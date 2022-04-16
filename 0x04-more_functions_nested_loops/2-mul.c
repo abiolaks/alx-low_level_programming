ae810de574b3c13afde85807423350a0dad01d0e
abiolaks <kingsleylawani@gmail.com>
2022-04-15 17:29:45 -0700

    MULTIPLY: times a and bthen rerurn the output to the function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - takes two argumnet times it
+ * @a: first input
+ * @b: second inpit
+ *
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
