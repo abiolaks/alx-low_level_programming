535bbfc0098c66f89b2a4cbdf3553215aa3db4f8
abiolaks <kingsleylawani@gmail.com>
2022-04-15 19:02:47 -0700

    Nexted loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,14 @@ void more_numbers(void)
 		int j;
 
 		for (j = 0; j <= 14; j++)
-			_putchar(i + '0');
+		{
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+
+			}
+		}
 	}
 	_putchar('\n');
 }
