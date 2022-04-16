25fdb38059eadc6e43f426db52b99e47c2eb0c35
abiolaks <kingsleylawani@gmail.com>
2022-04-15 18:56:44 -0700

    Nexted loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 1 to 14 10 times
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		int j;
+
+		for (j = 0; j <= 14; j++)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
