5a006c8b91147a401daba6c1714e889cfd2ffcb8
abiolaks <kingsleylawani@gmail.com>
2022-04-15 17:41:58 -0700

    NUMBER: display number from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print natural numbers on std out
+ */
+
+void print_numbers(void)
+{
+	int i
+
+	for (i = 0; i < 10; i++)
+		_putchar('i' + '0');
+	_putchar(012);
+}
