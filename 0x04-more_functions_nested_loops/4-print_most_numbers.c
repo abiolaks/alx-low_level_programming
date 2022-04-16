dee128f54f9ef57efa433762cf45b60134681f9d
abiolaks <kingsleylawani@gmail.com>
2022-04-15 18:43:34 -0700

    continue

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -this print number excluding 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 && i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+
+	}
+	_putchar('\n');
+}
