e0a0fee52f632abb1fb6e389acbc4cb4f328a910
abiolaks <kingsleylawani@gmail.com>
2022-04-20 17:39:05 -0700

    SWAP 0-001: swap the values oftwo interges

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - this swaps the values of two integer
+ * @a: first integer
+ * @b: second integer
+ *
+ * Description: This program manipulate memory address of two
+ * integer values by swapping the memory address.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
