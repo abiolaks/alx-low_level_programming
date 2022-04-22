4a8665c2e89d5d3fdfaee4b15093f4bbb76a2321
abiolaks <kingsleylawani@gmail.com>
2022-04-21 19:15:35 -0700

    Print Even chararacter

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - This function prints every even character in a string
+ * @str: This is a pointer to a char variable.
+ */
+
+void puts2(char *str)
+{
+	char *t;/* to hold the first address of str for iteration*/
+
+	for (t = str; *t != NULL; t++)
+	{
+		if ((*t % 2 == 0))/* the value in t is check to be even*/
+		{
+			_putchar(*t);/*print the element of display*/
+		}
+	}
+	_putchar('\n');
+}
