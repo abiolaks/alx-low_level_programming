2dff8f58c7d49ee29d487e61b4b3414a3b0b7f3f
abiolaks <kingsleylawani@gmail.com>
2022-04-25 02:48:19 -0700

    add two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - This is a function appends the src string to the dest
- * string, overwriting the terminating null byte at the end
- * of dest, then adds a terminating null byte
- * @dest, @src: takes two argument 
- *
- * Return: it returns a pointer to the resulting string dest.
- */
-
-/* function definiton */
-
-/* define a function to return the count length of string of dest
- * excluding the null byte
+ * _strlen - returns the length of string
+ * @str: string pass to function
+ * Description: _strlen return counts of char string
+ * Return: count of the string is return an int.
  */
 int _strlen(char *str)
 {
@@ -23,16 +15,27 @@ int _strlen(char *str)
 	{
 		size++;
 	}
-	return size;
+	return (size);
 
 }
+
+/**
+ * _strcat - append two pointers of char type.
+ * @dest: destination pointer
+ * @src: the char strng to append
+ *
+ * Return: new dest with src appended : a pointer of
+ * char type
+ */
 char *_strcat(char *dest, char *src)
 {
 	/*
 	 * get the length of dest excluding the null byte
 	 * and point to the last char byte (address in memory)
 	 */
+
 	int i = 0;
+
 	dest = dest + _strlen(dest);
 
 	/* iterate through src and append each to dest */
@@ -46,5 +49,5 @@ char *_strcat(char *dest, char *src)
 	}
 	/* add null byte at the end of str(the new dest)*/
 	*dest = '\0';
-	return dest;
+	return (dest);
 }
