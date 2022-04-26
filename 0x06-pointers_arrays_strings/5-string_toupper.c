8c7b74785c558a1931a9885bc075b36ca59d799b
abiolaks <kingsleylawani@gmail.com>
2022-04-25 21:57:57 -0700

    caps  the words

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** string_toupper - this capiliaxe wordes
+ * @c: the argument to capitatlize
+ *
+ * Return: this return a pointer to char
+ */
+
+char *string_toupper(char *c)
+{
+	int i;
+	for (i = 0; *(c + i) != 0; i++)
+	{
+		if (*(c + i) >= 'a' && *(c +i) <= 'z')
+			*(c + i) = *(c + i) - 32;
+	}
+	return (c);
+}
