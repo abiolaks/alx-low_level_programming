af46d9486f8f9398294c79192228d292239fc907
abiolaks <kingsleylawani@gmail.com>
2022-04-25 22:00:58 -0700

    caps  the words

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,10 @@
 char *string_toupper(char *c)
 {
 	int i;
+
 	for (i = 0; *(c + i) != 0; i++)
 	{
-		if (*(c + i) >= 'a' && *(c +i) <= 'z')
+		if (*(c + i) >= 'a' && *(c + i) <= 'z')
 			*(c + i) = *(c + i) - 32;
 	}
 	return (c);
