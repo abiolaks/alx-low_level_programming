d3997d0e20182595667632ee97b97f0b45260b08
abiolaks <kingsleylawani@gmail.com>
2022-04-27 20:53:49 -0700

    get a char from pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - This functions locates a character in a string
+ * @s: the string to search
+ * @c: the character byte to search for in s
+ *
+ * Return: it returns a pointer to the first occurence of
+ * character c in the string s, or null
+ * if none is found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i) != 0)
+	{
+		if (*(s + i) == c)
+			return (&s[i]);
+		i++;
+	} 
+	return (0);
+}
