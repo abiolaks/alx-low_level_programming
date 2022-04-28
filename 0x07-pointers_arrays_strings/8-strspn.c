b9d5b0abd78edeb9ed37391b5e4af707defd2080
abiolaks <kingsleylawani@gmail.com>
2022-04-27 21:22:35 -0700

    print a chessboard grid

---
+++ 0x07-pointers_arrays_strings/8-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strspn - This function gets the length of a prefix substring
+ * @s: string to get length of substring
+ * @accept: substring
+ *
+ * Return: this returns the number of bytes in the initial segment
+ * of s which consist only of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
