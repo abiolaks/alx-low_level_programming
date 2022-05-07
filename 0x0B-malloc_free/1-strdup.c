6e5871b5a7f29f958d8c56e227476024a966eefb
abiolaks <kingsleylawani@gmail.com>
2022-05-06 19:10:07 -0700

    copy string to a new memory location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - this function returns a copy of string argument.
+ * @str: the argument string to copy.
+ * Description: This function returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as
+ * parameter.
+ * Return: return null if str is null, return null if the allocated
+ * memory is null.
+ */
+
+char *_strdup(char *str)
+{
+	char *copy_str;
+	size_t i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		copy_str = (char *) malloc(sizeof(char));
+
+		if (copy_str != NULL)
+		{
+			while (*str)
+			{
+				*copy_str++ = *s++;
+			}
+			return (copy_str);
+		}
+		else
+			return (NULL);
+	}
+	free(copy_str);
+}
