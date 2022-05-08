de2cc370c881494e0ed9eabf10dd20b9f298464b
abiolaks <kingsleylawani@gmail.com>
2022-05-07 21:36:59 -0700

    added heaer forthe isdigt function

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - the entry point
@@ -28,7 +29,7 @@ int main(int argc, char *argv[])
 			while (argv[i][j] != '\0')
 			{
 
-				if (!(isdigit(argv[i][j]))jjjj)
+				if (!(isdigit(argv[i][j])))
 				{
 					printf("Error\n");
 					return (1);
