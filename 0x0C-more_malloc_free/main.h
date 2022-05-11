ed84b629727bee0bf667815977b9116e62202723
abiolaks <kingsleylawani@gmail.com>
2022-05-10 21:48:39 -0700

    reallocate memory size

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(size_t, size_t);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, size_t old_size, size_t new_size);
