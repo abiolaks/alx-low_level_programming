97989dd6ea0607efe5103ce58a738d22b3505234
abiolaks <kingsleylawani@gmail.com>
2022-05-10 22:01:35 -0700

    mull

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * _print - moves a string one place to the left and prints the string
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,3 +3,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(size_t, size_t);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, size_t old_size, size_t new_size);
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+int _putchar(char c);
