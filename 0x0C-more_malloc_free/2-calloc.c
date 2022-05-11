f2aceeb4b7cf3a9c8f9cd5fcd44069e41f305f34
abiolaks <kingsleylawani@gmail.com>
2022-05-10 20:04:18 -0700

    creating memory using calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "stdlib.h"
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of array element
+ * @size: size of the array element
+ *
+ * Return: returns a pointer to the allocated memory
+ */
+
+void *_calloc(size_t nmemb, size_t size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = calloc(nmemb, sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
