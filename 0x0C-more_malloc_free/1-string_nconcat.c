96929104bccbd0287819f296069c27073c876aa1
abiolaks <kingsleylawani@gmail.com>
2022-05-10 19:46:50 -0700

    coded cp

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,33 +10,32 @@
  */
 char *string_nconcat(char *s1, char *s2, size_t n)
 {
-	char *ptr;
-	size_t i = 0, j = 0, k = 0, z = 0;
+	unsigned int i, j, k;
+	char *s;
 
 	if (s1 == NULL)
 		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
 	if (s2 == NULL)
 		j = 0;
-	while (*s1++)
-		i++;
-	while (*s2++)
-		j++;
-	if (n >= j)
-		n = j;
-	ptr = malloc(sizeof(char) * (i + n + 1));
-	if (ptr == NULL)
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
 		return (NULL);
-	while (k < i)
-	{
-		ptr[k] = s1[k];
-		k++;
-	}
-	while (z < n)
-	{
-		ptr[k] = s2[z];
-		k++;
-		z++;
-	}
-	ptr[k] = '\0';
-	return (ptr);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
