ba2e457729d0b79d83d9a5c0c44deeb5db3ed7b2
abiolaks <kingsleylawani@gmail.com>
2022-05-11 15:33:16 -0700

    ADD doc for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - defines attributes for dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Return: return nothing
+ */
+
 struct dog
 {
 	char *name;
