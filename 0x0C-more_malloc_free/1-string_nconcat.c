c43bc76a7f9b252de44c495f4eb19542dcbd2255
abiolaks <kingsleylawani@gmail.com>
2022-05-10 19:44:34 -0700

    added empty string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, size_t n)
 	size_t i = 0, j = 0, k = 0, z = 0;
 
 	if (s1 == NULL)
-		*s1 = "";
+		i = 0;
 	if (s2 == NULL)
-		*s2 = "";
+		j = 0;
 	while (*s1++)
 		i++;
 	while (*s2++)
