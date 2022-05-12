585066674323966ca396c20f13580f8377442cf2
abiolaks <kingsleylawani@gmail.com>
2022-05-11 17:13:16 -0700

    create memory for d if empty and exit if malloc fails

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,7 +15,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	if (d == NULL)
 		exit(0);
 
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
