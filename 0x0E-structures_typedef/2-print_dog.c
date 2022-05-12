24dcc54c7f20b96fa5379fcf45aa709f7f07efcc
abiolaks <kingsleylawani@gmail.com>
2022-05-11 18:05:46 -0700

    Add the proper conditon

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,13 +12,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-	{
 		printf("Name: (nil)");
-	}
-	else if (d->owner == NULL)
-	{
-		printf("Owner: (nil)");
-	}
 	else
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
 }
