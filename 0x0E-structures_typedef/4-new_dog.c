57928c89307e41b4da09abfa31605e8cc7908e63
abiolaks <kingsleylawani@gmail.com>
2022-05-12 19:41:45 -0700

    add parameter doc

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * _strdup - fill a copy of string to memory allocated
- * @s: copy of string
+ * new_dog - fill a copy of string to memory allocated
+ * @name: copy of string name
+ * @age: hold the age
+ * @owner: hold the copy of the owner name
  *
- * Return: a pointer to string 
+ * Return: a pointer to string
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int nl, ol, i;
+	unsigned int name_length, owner_length, i;
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
@@ -17,29 +19,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	for (nl = 0; name[nl]; nl++)
+	for (name_length = 0; name[name_length]; name_length++)
 		;
-	nl++;
-	dog->name = malloc(nl * sizeof(char));
+	name_length++;
+	dog->name = malloc(name_length * sizeof(char));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < nl; i++)
+	for (i = 0; i < name_length; i++)
 		dog->name[i] = name[i];
 	dog->age = age;
-	for (ol = 0; owner[ol]; ol++)
+	for (owner_length = 0; owner[owner_length]; owner_length++)
 		;
-	ol++;
-	dog->owner = malloc(ol * sizeof(char));
+	owner_length++;
+	dog->owner = malloc(owner_length * sizeof(char));
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (i = 0; i < ol; i++)
+	for (i = 0; i < owner_length; i++)
 		dog->owner[i] = owner[i];
 	return (dog);
 }
