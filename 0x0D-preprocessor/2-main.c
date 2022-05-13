db166ecb150783a7664721f6a0cd26618736e158
abiolaks <kingsleylawani@gmail.com>
2022-05-12 20:15:47 -0700

    print file name using the macro __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: an int
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
