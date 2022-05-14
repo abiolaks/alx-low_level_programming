f568c9685b6e14a14015a41d764a2d44f66fa294
abiolaks <kingsleylawani@gmail.com>
2022-05-13 17:28:56 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - print a name
  * @name: a pointer to char
@@ -12,12 +13,9 @@ void print_name(char *name, void (*f)(char *))
 	 * checking the pass pointer that is not null
 	 * name and f
 	 */
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
-	{
+
+	if (name != NULL || f != NULL)
 		f(name);
-	}	
+	else
+		return;
 }
