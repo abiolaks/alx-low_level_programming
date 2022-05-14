5df97a5ea7d49b2773b77670bddf6a1509094608
abiolaks <kingsleylawani@gmail.com>
2022-05-13 17:14:16 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: a pointer to char
+ * @f: a pointer to a function
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	/*
+	 * checking the pass pointer that is not null
+	 * name and f
+	 */
+	if (name != NULL && f != NULL)
+		f(name);
+}
+
+
+
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_PTR_H
+#define FUNCTION_PTR_H
+void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
+#endif
