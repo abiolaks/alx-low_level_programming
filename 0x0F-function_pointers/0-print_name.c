c19bbf1e2be89ffd8fcca70b0a36f94a7ecfcf68
abiolaks <kingsleylawani@gmail.com>
2022-05-13 18:22:57 -0700

    addded block

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,20 @@
+#include <stdio.h>
 #include "function_pointers.h"
-#include <stdlib.h>
+
 /**
- * print_name - print a name
- * @name: a pointer to char
- * @f: a pointer to a function
- *
- * Return: nothing
- */
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is pointer
+ * Return: Nothing.
+**/
 void print_name(char *name, void (*f)(char *))
 {
-	/*
-	 * checking the pass pointer that is not null
-	 * name and f
-	 */
-
-	if (name != NULL || f != NULL)
+	if (name == NULL || f == NULL)
 	{
-		f(name)
+		return;
 	}
-	return;
+	else
+	{
+		f(name);
+	}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
-#ifndef FUNCTION_PTR_H
-#define FUNCTION_PTR_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+
 #endif
