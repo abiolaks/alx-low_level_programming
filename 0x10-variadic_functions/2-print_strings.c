4a54085ad874e2ea425f54bf37b9bd2dd1d9f8d8
abiolaks <kingsleylawani@gmail.com>
2022-05-16 21:32:08 -0700

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_ptrings - prints ptrings.
+ * @separator: ptring to be printed between the ptrings.
+ * @n: number of ptrings passed to the function.
+ *
+ * Return: nothing.
+ */
+void print_ptrings(const char *separator, const unsigned int n, ...)
+{
+	va_list arg_param;
+	unsigned int i;
+	char *ptr;
+
+	va_start(arg_param, n);
+
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(arg_param, char *);
+
+		if (ptr)
+			printf("%s", ptr);
+		else
+			printf("(nil)");
+
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(arg_param);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -5,3 +5,4 @@
 ## Task
 0.sum_them_all.c - This function returns the sum of all its parameter
 1-print_numbers.c  - print number separated by a delimter
+2-print_strings.c - print string
