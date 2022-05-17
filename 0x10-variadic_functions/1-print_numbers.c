ca38c97d9f1a2bd383fc0673dd1848a8fb643c87
abiolaks <kingsleylawani@gmail.com>
2022-05-16 19:49:58 -0700

    print numbers by delimeter

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints number, followed linefeed
+ * @separator: to demacate the digit
+ * @n: argcount
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	size_t i;
+
+	if (separator == 0)
+		return;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (i < (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -4,3 +4,4 @@
 
 ## Task
 0.sum_them_all.c - This function returns the sum of all its parameter
+1-print_numbers.c  - print number separated by a delimter
