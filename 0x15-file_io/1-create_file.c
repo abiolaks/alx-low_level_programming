67d34ab263ba7922bd077791f143035436a1b596
abiolaks <kingsleylawani@gmail.com>
2022-06-01 21:43:23 -0700

    create file with content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,17 +11,25 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	size_t i = 0;
+	int fw;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		fd = creat(filename, 0600);
-	else
-		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	return (fd);
+	while (*(text_content + i) != 0)
+		i++;
+	fw = write(fd, text_content, i);
+	if (fw == -1)
+		return (-1);
 
 	close(fd);
+
+	return (1);
 
 }
